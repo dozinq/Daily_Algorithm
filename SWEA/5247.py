f4ab9092013d952b04bd9ced84d321a63d3360ec
dozinq <dojinuk@kakao.com>
2022-04-05 09:11:16 +0900

    Create 5247.py

--- SWEA/5247.py
+++ SWEA/5247.py
@@ -1,29 +1,43 @@
 import sys
-
 sys.stdin = open('input.txt')
+from collections import deque
 
+def cal(n):
+    global ans
+    cnt = 0
+    deq = deque([n])
+    num_lst = [0] * 1000001
+    tmp_lst = deque()
 
-def max_edge(num, dist):
-    ans_lst.append(dist)
-    for n in data[num]:
-        if not visited[n]:
-            visited[n] = 1
-            max_edge(n, dist + 1)
-            visited[n] = 0
+    while not ans:
+        while deq:
+            num = deq.popleft()
+            if num == M:
+                ans = cnt
+                return
+            a, b, c, d = num + 1, num * 2, num - 1, num - 10
+            if 0 <= a <= 1000000 and not num_lst[a]:
+                tmp_lst.append(a)
+                num_lst[a] = True
+            if 0 <= b <= 1000000 and not num_lst[b]:
+                tmp_lst.append(b)
+                num_lst[b] = True
+            if 0 <= c <= 1000000 and not num_lst[c]:
+                tmp_lst.append(c)
+                num_lst[c] = True
+            if 0 <= d <= 1000000 and not num_lst[d]:
+                tmp_lst.append(d)
+                num_lst[d] = True
+        cnt += 1
+        deq, tmp_lst = tmp_lst, deq
 
+    return
 
-for tc in range(1, int(input()) + 1):
+T = int(input())
+
+for tc in range(1, T+1):
     N, M = map(int, input().split())
-    data = [[] for _ in range(N + 1)]
+    ans = 0
+    cal(N)
 
-    for _ in range(M):
-        node1, node2 = map(int, input().split())
-        data[node1].append(node2)
-        data[node2].append(node1)
-
-    ans_lst = []
-    for i in range(1, N + 1):
-        visited = [0] * (N+1)
-        visited[i] = 1
-        max_edge(i, 1)
-    print(f'#{tc} {max(ans_lst)}')
+    print(f'#{tc} {ans}')
