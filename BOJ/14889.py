8c6f2810364bca21f0e620878cbd9c644949ab98
dozinq <dojinuk@kakao.com>
2022-05-01 21:31:19 +0900

    Create 220501

---
+++ BOJ/14889.py
@@ -0,0 +1,14 @@
+import sys
+from itertools import combinations
+
+def team_maker(n):
+    pass
+
+def team_status(n):
+    pass
+
+sys.stdin = open('input.txt')
+
+N = int(input())
+data = [list(map(int, input().split())) for _ in range(N)]
+
