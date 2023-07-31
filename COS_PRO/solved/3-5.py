bace3e48a4165df4194418f73df7cc833dc892cd
dozinq <dojinuk@kakao.com>
2023-08-01 00:29:07 +0900

    Create 3-5.py

--- COS_PRO/solved/3-5.py
+++ COS_PRO/solved/3-5.py
@@ -4,11 +4,14 @@
 def solution(phrases, second):
     # 여기에 코드를 작성해주세요.
     answer = ''
+    second %= 28
+    phrases = '______________' + phrases + '______________'
+    answer = phrases[second:second+14]
     return answer
 
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
 phrases = "happy-birthday"
-second = 3
+second = 29
 ret = solution(phrases, second)
 
 #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
