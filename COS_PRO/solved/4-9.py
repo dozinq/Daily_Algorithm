cd1cdc9668a9256ac591d73574beed6e95c68b23
dozinq <dojinuk@kakao.com>
2023-08-03 00:39:16 +0900

    Create 4-9.py

---
+++ COS_PRO/solved/4-9.py
@@ -0,0 +1,17 @@
+# 다음과 같이 import를 사용할 수 있습니다.
+# import math
+
+def solution(hour, minute):
+    answer = ''
+    hour = (360 / 12) * hour + minute *0.5
+    minute = (360 / 60) * minute
+    answer = str(abs(hour - minute))
+    return answer
+
+# 아래는 테스트케이스 출력을 해보기 위한 코드입니다.
+hour = 3
+minute = 0
+ret = solution(hour, minute)
+
+# [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은 ", ret, " 입니다.")
